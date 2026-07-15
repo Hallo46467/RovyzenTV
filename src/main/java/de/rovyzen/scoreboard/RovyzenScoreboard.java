40b17e37e9119f27183002654d5a05965f2bad2f
Hallo46467 <162601313+Hallo46467@users.noreply.github.com>
2026-07-15 21:47:50 +0200

    Connect scoreboard with config and placeholders

--- src/main/java/de/rovyzen/scoreboard/RovyzenScoreboard.java
+++ src/main/java/de/rovyzen/scoreboard/RovyzenScoreboard.java
@@ -2,6 +2,7 @@ package de.rovyzen.scoreboard;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.DisplaySlot;
@@ -10,27 +11,40 @@ import org.bukkit.scoreboard.Scoreboard;
 
 public class RovyzenScoreboard extends JavaPlugin {
 
+    private FileConfiguration config;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        config = getConfig();
         getLogger().info("RovyzenScoreboard aktiviert!");
+
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
                 createScoreboard(player);
             }
-        }, 0L, 40L);
+        }, 0L, config.getLong("scoreboard.update-time", 40L));
     }
 
     private void createScoreboard(Player player) {
         Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
-        Objective obj = board.registerNewObjective("rovyzen", "dummy", ChatColor.DARK_PURPLE + "RovyzenTV");
+        String title = color(config.getString("scoreboard.title", "&5&lRovyzenTV"));
+        Objective obj = board.registerNewObjective("rovyzen", "dummy", title);
         obj.setDisplaySlot(DisplaySlot.SIDEBAR);
 
-        obj.getScore(ChatColor.GRAY + "------------").setScore(5);
-        obj.getScore(ChatColor.WHITE + "Spieler: " + ChatColor.GREEN + Bukkit.getOnlinePlayers().size()).setScore(4);
-        obj.getScore(ChatColor.WHITE + "Name: " + ChatColor.AQUA + player.getName()).setScore(3);
-        obj.getScore(ChatColor.GOLD + "Rovyzen Network").setScore(2);
-        obj.getScore(ChatColor.GRAY + "------------").setScore(1);
+        int score = config.getStringList("scoreboard.lines").size();
+        for (String line : config.getStringList("scoreboard.lines")) {
+            line = line.replace("%players%", String.valueOf(Bukkit.getOnlinePlayers().size()))
+                    .replace("%player%", player.getName())
+                    .replace("%ping%", "0")
+                    .replace("%rank%", "Spieler");
+            obj.getScore(color(line)).setScore(score--);
+        }
 
         player.setScoreboard(board);
+    }
+
+    private String color(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
     }
 }
