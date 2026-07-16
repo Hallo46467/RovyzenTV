5f7510beb7577503b1f2b64a8b0e68ea3a3df0be
Hallo46467 <162601313+Hallo46467@users.noreply.github.com>
2026-07-16 18:10:22 +0200

    Connect managers listeners and commands in main class

--- src/main/java/de/rovyzen/scoreboard/RovyzenScoreboard.java
+++ src/main/java/de/rovyzen/scoreboard/RovyzenScoreboard.java
@@ -1,18 +1,38 @@
 package de.rovyzen.scoreboard;
 
+import de.rovyzen.scoreboard.command.ReloadCommand;
+import de.rovyzen.scoreboard.command.ScoreboardCommand;
+import de.rovyzen.scoreboard.command.ToggleCommand;
 import de.rovyzen.scoreboard.config.ConfigManager;
+import de.rovyzen.scoreboard.listener.JoinListener;
+import de.rovyzen.scoreboard.listener.QuitListener;
+import de.rovyzen.scoreboard.manager.ScoreboardManager;
+import de.rovyzen.scoreboard.manager.TablistManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RovyzenScoreboard extends JavaPlugin {
 
     private static RovyzenScoreboard instance;
     private ConfigManager configManager;
+    private ScoreboardManager scoreboardManager;
+    private TablistManager tablistManager;
 
     @Override
     public void onEnable() {
         instance = this;
+
         configManager = new ConfigManager(this);
         configManager.load();
+
+        scoreboardManager = new ScoreboardManager(this);
+        tablistManager = new TablistManager(this);
+
+        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
+        getServer().getPluginManager().registerEvents(new QuitListener(), this);
+
+        getCommand("scoreboard").setExecutor(new ScoreboardCommand());
+        getCommand("scoreboardreload").setExecutor(new ReloadCommand(this));
+        getCommand("toggle").setExecutor(new ToggleCommand());
 
         getLogger().info("RovyzenScoreboard v2.0 aktiviert!");
     }
@@ -28,5 +48,13 @@ public class RovyzenScoreboard extends JavaPlugin {
 
     public ConfigManager getConfigManager() {
         return configManager;
+    }
+
+    public ScoreboardManager getScoreboardManager() {
+        return scoreboardManager;
+    }
+
+    public TablistManager getTablistManager() {
+        return tablistManager;
     }
 }
